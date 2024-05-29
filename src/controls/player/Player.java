d99ae6113b616931d37545e9189e933d408269e8
nhhsag12 <nhhoang12122004@gmail.com>
2024-05-30 01:25:44 +0700

    Add some important atributes and methods

--- src/controls/player/Player.java
+++ src/controls/player/Player.java
@@ -1,55 +1,98 @@
 package controls.player;
+import java.util.ArrayList;
 
+import controls.board.*;
 
 public class Player {
+	private int playerID;
+	private int point;
+	private ArrayList<BoardSquare> validBoardSquare;
+
+    public Player(int playerID, int point) {
+		super();
+		this.playerID = playerID;
+		this.point = point;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setValidBoardSquare(ArrayList<BoardSquare> validBoardSquare) {
+		this.validBoardSquare = validBoardSquare;
+	}
 
 
-    public void reset() {
-
+	public void makeMove(Board b, int choosenSquareID, boolean isLeftMove) {
+    	// Whether the choosen square is in valid board square of the player
+    	int flag = 0;
+    	for(BoardSquare i : this.validBoardSquare) {
+    		if(i.getboardSquareID()==choosenSquareID) {
+    			flag += 1;
+    			break;
+    		}
+    	}
+    	if(b.getListOfSquare().get(choosenSquareID).getNumberOfCitizens()>0) {
+    		flag += 1;
+    	}
+    	if(flag == 2) {
+    		ArrayList<BoardSquare> bss = b.getListOfSquare();
+    		BoardSquare choosenSquare = b.getListOfSquare().get(choosenSquareID);
+    		int citizens = choosenSquare.getNumberOfCitizens();
+    		int currentSquareID = choosenSquareID;
+    		while(citizens>0) {
+    			citizens--;
+    			if(isLeftMove == true) {
+    				if(currentSquareID == 11) currentSquareID = 0;
+    				else currentSquareID++;
+    				bss.get(currentSquareID).setNumberOfCitizens(bss.get(currentSquareID).getNumberOfCitizens()+1);
+    			}else {
+    				if(currentSquareID == 0) currentSquareID = 11;
+    				else currentSquareID--;
+    				bss.get(currentSquareID).setNumberOfCitizens(bss.get(currentSquareID).getNumberOfCitizens()+1);
+    			}
+    			
+    			// decide if the turn is continued or stopped (get point or not)
+    			if(citizens==0) {
+    				if(isLeftMove == true) {
+    					if(currentSquareID == 11) currentSquareID = 0;
+        				else currentSquareID++;
+    					if(bss.get(currentSquareID).getNumberOfCitizens()>0) {
+    						// continue
+    						citizens = bss.get(currentSquareID).getNumberOfCitizens();
+    						bss.get(currentSquareID).setNumberOfCitizens(0);
+    					}else if(bss.get(currentSquareID).getNumberOfCitizens()==0) {
+    						// get point
+    						if(currentSquareID == 11) currentSquareID = 0;
+            				else currentSquareID++;
+    						if(bss.get(currentSquareID).getNumberOfCitizens()>0) {
+    							this.point+=bss.get(currentSquareID).getNumberOfCitizens();
+    							bss.get(currentSquareID).setNumberOfCitizens(0);
+    						}
+    					}
+    				}else {
+    					if(currentSquareID == 0) currentSquareID = 11;
+        				else currentSquareID--;
+    					if(bss.get(currentSquareID).getNumberOfCitizens()>0) {
+    						// continue
+    						citizens = bss.get(currentSquareID).getNumberOfCitizens();
+    						bss.get(currentSquareID).setNumberOfCitizens(0);
+    					}else if(bss.get(currentSquareID).getNumberOfCitizens()==0) {
+    						// get point
+    						if(currentSquareID == 0) currentSquareID = 11;
+            				else currentSquareID--;
+    						if(bss.get(currentSquareID).getNumberOfCitizens()>0) {
+    							this.point+=bss.get(currentSquareID).getNumberOfCitizens();
+    							bss.get(currentSquareID).setNumberOfCitizens(0);
+    						}
+    					}
+    				}
+    			}
+    		}
+    	}
     }
-
-    public void makeMove(Board b) {
-
-    }
-
-    public boolean isTurn() {
-    }
-
-    public void raiSoi(Board b) {
-    }
-
-
-    /*public int moveCase(Board b, BoardCell cur, BoardCell next, BoardCell after) {
-
-    } */
-
-    public int getPoint() {
-
-    }
-
-    public int getNumberTakenStones() {
-
-    }
-
-    public void setCurIndex(int curIndex) {
-
-    }
-
-    public void setDir(int dir) {
-
-    }
-
-    public boolean isValidMove(int ci) {
-
-    }
-
-    public void moveSetup(int ci, int d) {
-
-    }
-
-    public int getPlayerId() {
-
-    }
+    
+    
 }
 
 
