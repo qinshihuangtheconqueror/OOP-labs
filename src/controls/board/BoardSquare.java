980ca7a56dce1dbbcda606c942b17216f103bc1a
nhhsag12 <nhhoang12122004@gmail.com>
2024-05-29 23:04:48 +0700

    Update board square

--- src/controls/board/BoardSquare.java
+++ src/controls/board/BoardSquare.java
@@ -1,17 +1,22 @@
 package controls.board;
 
 abstract class BoardSquare {
-    int BoardSquare_ID;
-    int numberOfCitizens;
+    private int BoardSquare_ID;
+    private int numberOfCitizens;
 
     public BoardSquare(int BoardSquare_ID, int numberOfCitizens) {
         this.BoardSquare_ID = BoardSquare_ID;
         this.numberOfCitizens = numberOfCitizens;
     }
 
-    public int getNumberOfStones() {
-        return numberOfCitizens;
+    public int getNumberOfCitizens() {
+        return this.numberOfCitizens;
     }
-
+    
+    public void setNumberOfCitizens(int i) {
+    	this.setNumberOfCitizens(i);
+    }
+    
+    abstract public boolean isEmpty();
     abstract void operation();
 }
